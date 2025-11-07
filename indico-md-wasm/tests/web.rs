e0bc21e6e35977d8f3f286fd8b4a4ea0ca13821f
Pedro Ferreira <pedro@turbofish.cc>
2025-11-07 16:29:10 +0100

    Initial commit

---
+++ indico-md-wasm/tests/web.rs
@@ -0,0 +1,61 @@
+//! Test suite for the Web and headless browsers.
+
+#![cfg(target_arch = "wasm32")]
+
+extern crate wasm_bindgen_test;
+use indico_md_wasm::indico_markdown;
+use js_sys::{Array, RegExp};
+use wasm_bindgen::JsValue;
+use wasm_bindgen_test::*;
+
+#[wasm_bindgen_test]
+fn function_test() {
+    let md = r#"## TEST
+ * TKT1234567: solved
+ * Still checking gh:123
+ * [gh:124](https://somewhere.else) shouldn't be autolinked
+"#;
+    let rules = Array::new();
+    rules.push(&Array::of2(
+        &RegExp::new(r"\bTKT(\d{7})\b", ""),
+        &JsValue::from("https://tkt.sys/{1}"),
+    ));
+    rules.push(&Array::of2(
+        &RegExp::new(r"\bgh:(\d+)\b", ""),
+        &JsValue::from("https://github.com/indico/indico/issues/{1}"),
+    ));
+
+    let res = indico_markdown(md, &rules.into()).unwrap();
+
+    assert_eq!(
+        res,
+        r##"<h2><a href="#test" aria-hidden="true" class="anchor" id="indico-md-test"></a>TEST</h2>
+<ul>
+<li><a href="https://tkt.sys/1234567" title="TKT1234567">TKT1234567</a>: solved</li>
+<li>Still checking <a href="https://github.com/indico/indico/issues/123" title="gh:123">gh:123</a></li>
+<li><a href="https://somewhere.else">gh:124</a> shouldn't be autolinked</li>
+</ul>
+"##
+    );
+}
+
+#[wasm_bindgen_test]
+fn interface_test() {
+    assert_eq!(indico_markdown("", &Array::new()), Ok("".into()));
+
+    let rules = Array::new();
+    rules.push(&Array::of2(
+        &RegExp::new(r"/a/", ""),
+        // URL cannot be a bool, so this should fail
+        &JsValue::from_bool(true),
+    ));
+    let res = indico_markdown("foo", &rules);
+    assert!(res.is_err());
+    assert!(
+        res.err()
+            .unwrap()
+            .as_string()
+            .expect("Error is not a string")
+            .contains("not a valid string")
+    )
+}
